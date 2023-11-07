9a1d5736875f52ec300a59cefc8ecdee37f2b1d4
kimjehyeon <dlsrks0631@naver.com>
2023-11-07 23:40:57 +0900

    update

--- calculator.py
+++ calculator.py
@@ -29,8 +29,8 @@ while True:
 
         break
 
-    # 현재 입력이 정수이거나 + 일 경우
-    elif now_input.isdigit() or now_input == '+':
+    # 현재 입력이 정수이고 + 일 경우
+    elif now_input.isdigit() and now_input == '+':
         accumulated_input += now_input
 
     # 예외
