f194ed884a814a57ebaaa8105842ee481c94bf40
201812722 <201812722@jbnu.ac.kr>
2023-11-09 23:51:07 +0900

    Update calculator.py
    코드 리팩토링: 연산자 클래스 구체적 명시, 에러 처리 명시 출력, 이스터에그 출력 수정

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-class op: # 연산자 클래스
+class Calculater_operater: # 연산자 클래스 Calculater_operater로 정의.
     def add(num1, num2): # 덧셈.
         return int(num1) + int(num2)
 
@@ -15,8 +15,8 @@ def main():
     if (int(result) != result): # 에러 처리(정수가 아닌 경우).
         iserror = True
 
-    if result == 321654987: # 이스터에그
-        print('Hello!')
+    if result == 321654987: # 이스터에그 코드.
+        print('Hello! This Is Yeonhyo Easter Egg!!')
         return
     # Calculator
     while True: # 등호나 잘못된 부호가 나올 때까지 무한 반복.
@@ -33,22 +33,22 @@ def main():
         else: # '+, -, *' 입력.
             operand = input()
             if(operand in ['=', '+', '-', '*']): #에러 처리(연산자가 연달아 입력되었을 경우).
-                print('ERROR!')
+                print('연산자 중복입력으로 인한 ERROR!')
                 break
             operand = float(operand)
             if (int(operand) != operand): # 에러 처리(정수가 아닌 경우).
                 iserror = True
 
-            if operand == 321654987: # 이스터에그
-                print('Hello!')
+            if operand == 321654987: # 이스터에그 코드.
+                print('Hello! This Is Team Yeonhyo Easter Egg!!')
                 break
 
             if operator == '+': # 덧셈.
-                result = op.add(result, operand)
+                result = Calculater_operater.add(result, operand)
             elif operator == '-': # 뺄셈.
-                result = op.sub(result, operand)
+                result = Calculater_operater.sub(result, operand)
             elif operator == '*': # 곱셈.
-                result = op.mul(result, operand)    
+                result = Calculater_operater.mul(result, operand)    
 
 if __name__ == "__main__":
     main()
