d7ecac7a5bb2ad6c2a10a5b53399f652941999cb
Jaeho-Jung <66736234+Jaeho-Jung@users.noreply.github.com>
2023-11-16 12:45:47 +0900

    refactor: rename Operator class, add operate dict

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ EASTER_EGG_CODE = '987654321987654321'
 ERROR_MESSAGE = "ERROR!"
 EASTER_EGG_MESSAGE = "Hello! This Is Team Yeonhyo Easter Egg!!"
 
-class Calculater_operater: # 연산자 클래스 Calculater_operater로 정의.
+class Operator: # 연산자 클래스 Calculater_operater로 정의.
     def add(num1, num2): # 덧셈.
         return int(num1) + int(num2)
 
@@ -12,6 +12,13 @@ class Calculater_operater: # 연산자 클래스 Calculater_operater로 정의.
 
     def mul(num1, num2): # 곱셈.
         return int(num1) * int(num2)
+    
+    # 연산자 메소드 딕셔너리
+    operate = { 
+        '+': add,
+        '-': sub,
+        '*': mul,
+    }
     
 def main():
     # Variables
@@ -26,7 +33,7 @@ def main():
     # Calculator
     while True: # 등호나 잘못된 부호가 나올 때까지 무한 반복.
         operator = input()
-        if(operator not in ['=', '+', '-', '*']): # 에러 처리('+, -, *'가 아닌 경우).
+        if(operator not in Operator.operate and operator != '='): # 에러 처리('+, -, *'가 아닌 경우).
             iserror = True
         
         if operator == '=': # '=' 입력.
@@ -37,7 +44,7 @@ def main():
             break
         else: # '+, -, *' 입력.
             operand = input()
-            if(operand in ['=', '+', '-', '*']): #에러 처리(연산자가 연달아 입력되었을 경우).
+            if(operand in Operator.operate and operator != '='): #에러 처리(연산자가 연달아 입력되었을 경우).
                 print(ERROR_MESSAGE)
                 break
             operand = float(operand)
@@ -48,12 +55,7 @@ def main():
                 print(EASTER_EGG_MESSAGE)
                 break
 
-            if operator == '+': # 덧셈.
-                result = Calculater_operater.add(result, operand)
-            elif operator == '-': # 뺄셈.
-                result = Calculater_operater.sub(result, operand)
-            elif operator == '*': # 곱셈.
-                result = Calculater_operater.mul(result, operand)    
+            result = Operator.operate[operator](result, operand)
 
 if __name__ == "__main__":
     main()
