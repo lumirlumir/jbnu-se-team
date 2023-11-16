508b3df79a608357fe12135cec43fe8bb9b06e73
Jaeho-Jung <66736234+Jaeho-Jung@users.noreply.github.com>
2023-11-16 12:53:45 +0900

    refactor: define is_integer() using re

--- calculator.py
+++ calculator.py
@@ -1,7 +1,14 @@
+import re
+
 # 상수
 EASTER_EGG_CODE = '987654321987654321'
 ERROR_MESSAGE = "ERROR!"
 EASTER_EGG_MESSAGE = "Hello! This Is Team Yeonhyo Easter Egg!!"
+
+# 정규표현식을 이용한 정수 확인 함수
+def is_integer(str):
+    """Returns True if the string is an integer."""
+    return re.match("[-]?\d+$", str) != None
 
 class Operator: # 연산자 클래스 Calculater_operater로 정의.
     def add(num1, num2): # 덧셈.
@@ -23,8 +30,8 @@ class Operator: # 연산자 클래스 Calculater_operater로 정의.
 def main():
     # Variables
     iserror = False # 에러 값.
-    result = float(input()) # 결과 값.
-    if (int(result) != result): # 에러 처리(정수가 아닌 경우).
+    result = input() # 결과 값.
+    if not is_integer(result): # 에러 처리(정수가 아닌 경우).
         iserror = True
 
     if result == EASTER_EGG_CODE: # 이스터에그 코드.
@@ -47,9 +54,9 @@ def main():
             if(operand in Operator.operate and operator != '='): #에러 처리(연산자가 연달아 입력되었을 경우).
                 print(ERROR_MESSAGE)
                 break
-            operand = float(operand)
-            if (int(operand) != operand): # 에러 처리(정수가 아닌 경우).
-                iserror = True
+            if not is_integer(operand): # 에러 처리(정수가 아닌 경우).
+                print(ERROR_MESSAGE)
+                break
 
             if operand == EASTER_EGG_CODE: # 이스터에그 코드.
                 print(EASTER_EGG_MESSAGE)
