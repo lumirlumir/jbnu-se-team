b9f566bf1a0af1eb22737973ee54bc3fe1047c68
kimjehyeon <dlsrks0631@naver.com>
2023-12-15 17:17:54 +0900

    팩토리얼 연산 기능 추가

--- calculator.py
+++ calculator.py
@@ -6,11 +6,9 @@ from operatorasm import *
 
 # 정규표현식을 이용한 정수 확인 함수
 def is_integer(str):
-    """Returns True if the string is an integer."""
     return re.match("[-]?\d+$", str) != None
     
 def get_user_input():
-    """Gets user inpus and return input as a list"""
     user_inputs = []
     user_input = None
 
@@ -32,85 +30,78 @@ def get_user_input():
 
 def has_error(user_inputs):
     # 사용자 입력이 없는 경우 ('='만 입력된 경우)
-    is_empty = len(user_inputs) == 0
-    # 한 종류의 연산자만 입력된 경우
-    is_operator_unique = not is_empty and all(element == user_inputs[1] for element in user_inputs[1::2])
-    # 마지막 입력이 연산자인 경우
-    is_operator_last_elem = not is_empty and user_inputs[-1] in Operator.operate
+    is_empty = len(user_inputs) == 0  # 이 줄이 누락되었을 수 있습니다.
+
+    # 마지막 입력이 연산자인 경우 (팩토리얼 제외)
+    is_operator_last_elem = not is_empty and user_inputs[-1] in Operator.operate and user_inputs[-1] != '!'
+
     # 모든 짝수 번째 요소들이 정수인 경우
     is_integer_even_elem = all(is_integer(element) for element in user_inputs[0::2])
-    # 모든 홀수 번째 요소들이 연산자인 경우
+
+    # 모든 홀수 번째 요소들이 연산자인 경우 (팩토리얼 포함)
     is_operator_odd_elem = all((element in Operator.operate) for element in user_inputs[1::2])
+
+    # 팩토리얼 연산자 '!'가 올바르게 사용되었는지 검증하는 로직
+    is_factorial_used_correctly = all(
+        (i == 0 or user_inputs[i-1] not in Operator.operate) and 
+        (i == len(user_inputs) - 1 or user_inputs[i+1] not in Operator.operate)
+        for i, element in enumerate(user_inputs) if element == '!'
+    )
     
-    return is_empty or is_operator_last_elem or not (is_operator_unique and is_integer_even_elem and is_operator_odd_elem)
+    return (is_empty or 
+            is_operator_last_elem or 
+            not (is_integer_even_elem and is_operator_odd_elem) or
+            not is_factorial_used_correctly)
+    
+    # 팩토리얼 연산자 '!'가 올바르게 사용되었는지 검증하는 로직
+    is_factorial_used_correctly = not any(
+        element == '!' and (i == len(user_inputs) - 1 or user_inputs[i + 1] in Operator.operate)
+        for i, element in enumerate(user_inputs)
+    )
+    
+    return is_empty or is_operator_last_elem or not (is_integer_even_elem and is_operator_odd_elem) or not is_factorial_used_correctly
 
-# 사용자 입력을 이용해 계산하고 결과를 반환한다.
 def calculate(user_inputs):
-    """Run calculator and return the result."""
-    result = None # 계산 결과
-    operand = None # 피연산자
-    operator = None # 연산자
+    result = None  # 계산 결과
+    operand = None  # 피연산자
+    operator = None  # 연산자
 
-    '''
-    반복문 Logic
-    1. 사용자 입력
-    2. 입력에 따른 처리
-        - 피연산자, 연산자가 번갈아 입력된다고 가정
-        2.1. 피연산자가 입력된 경우
-            2.1.1. 피연산자가 처음 입력된 경우
-                - result에 입력된 피연산자 저장
-            2.1.2. 피연산자와 연산자가 저장된 상태에서 피연산자가 입력된 경우
-                - 저장된 result, operand, operator를 이용해 연산
-                - 결과를 result에 저장
-        2.2. 연산자가 입력된 경우
-            - 입력받은 연산자를 operator 변수에 저장
-        2.3. 등호가 입력된 경우
-            - 결과 반환
-    '''
     for user_input in user_inputs:
-        # 등호가 입력된 경우 결과 출력
-        if user_input == '=': 
-            break
         # 정수가 입력된 경우
-        elif is_integer(user_input): 
-            # 사용자 입력을 피연산자 변수에 저장
-            operand = int(user_input) 
-            
-            # 피연산자가 처음으로 입력된 경우 입력을 result에 저장
-            if result == None: 
+        if is_integer(user_input):
+            operand = int(user_input)
+            if operator:
+                # 연산자가 팩토리얼일 경우
+                if operator == '!':
+                    result = Operator.operate[operator](operand)
+                    operand = None
+                else:
+                    result = Operator.operate[operator](result, operand)
+                    operand = None
+            else:
                 result = operand
-            else: 
-                # 저장된 연산자와 피연산자를 이용해 연산 후 결과를 result에 저장
-                result = Operator.operate[operator](result, operand)
-            
-            # 연산 후 연산자/피연산자 변수 초기화
-            operator = None 
-            operand = None 
-
         # 연산자가 입력된 경우
-        elif user_input in Operator.operate: 
-            # 사용자 입력을 연산자 변수에 저장
-            operator = user_input 
+        elif user_input in Operator.operate:
+            if user_input == '!':
+                if operand is not None:
+                    result = Operator.operate[user_input](operand)
+                    operand = None
+            else:
+                operator = user_input
 
     return result
 
+
 def run_calculator():
-    """Run calculator and return the result."""
-    # 사용자 입력
     try:
         user_inputs = get_user_input()
-    # 이스터에그 코드 입력 시 즉시 이스터에그 메시지 출력
     except EasterEggException as message:
         return message
     
-    # 입력 오류 존재 시 오류 메시지 반환
     if has_error(user_inputs):
         return ERROR_MESSAGE
 
-    # 입력 식 계산
     result = calculate(user_inputs)
-
-    # 계산 결과 반환
     return result
 
 def main():
--- operatorasm.py
+++ operatorasm.py
@@ -1,4 +1,5 @@
 # operatorasm.py
+import math
 
 class Operator: # 연산자 클래스 Calculater_operater로 정의.
     def add(num1, num2): # 덧셈.
@@ -10,9 +11,16 @@ class Operator: # 연산자 클래스 Calculater_operater로 정의.
     def mul(num1, num2): # 곱셈.
         return int(num1) * int(num2)
     
+    @staticmethod
+    def factorial(num):
+        if num < 0:
+            raise ValueError("팩토리얼은 음수에 대해 정의할 수 없음")
+        return math.factorial(num)
+    
     # 연산자 메소드 딕셔너리
     operate = { 
         '+': add,
         '-': sub,
         '*': mul,
+        '!': factorial
     }
