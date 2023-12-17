c8c6dc1071bc2933a047d95f398acfa6362cf872
Jaeho-Jung <66736234+Jaeho-Jung@users.noreply.github.com>
2023-12-17 14:28:27 +0900

    test: add unit test of Operator class - negative

--- tests/test_operator.py
+++ tests/test_operator.py
@@ -13,17 +13,17 @@ class TestAdd(unittest.TestCase):
     Test the add function from the operatorasm library
     """
 
-
     # Validation Testing
     def test_add_integers(self):
         self.assertEqual(Operator.add(1, 2), 3)
+        self.assertEqual(Operator.add(4, -5), -1)
         self.assertNotEqual(Operator.add(4, 5), 6)
 
-    # Defect Testing
+    # Defect Testing - floats
     def test_add_floats(self):
         self.assertRaises(NotIntegerException, Operator.add, 10.5, 2)
 
-    # Defect Testing
+    # Defect Testing - strings
     def test_add_strings(self):
         self.assertRaises(NotIntegerException, Operator.add, 'abc', 'def')
 
@@ -35,13 +35,14 @@ class TestSub(unittest.TestCase):
     # Validation Testing
     def test_sub_integers(self):
         self.assertEqual(Operator.sub(3, 2), 1)
+        self.assertEqual(Operator.sub(4, -5), 9)
         self.assertNotEqual(Operator.sub(19, 5), 10)
 
-    # Defect Testing
+    # Defect Testing - floats
     def test_sub_floats(self):
         self.assertRaises(NotIntegerException, Operator.sub, 10.5, 2)
     
-    # Defect Testing
+    # Defect Testing - strings
     def test_sub_strings(self):
         self.assertRaises(NotIntegerException, Operator.sub, 'abc', 'def')
 
@@ -53,15 +54,16 @@ class TestMul(unittest.TestCase):
     # Validation Testing
     def test_mul_integers(self):
         self.assertEqual(Operator.mul(3, 2), 6)
-        self.assertNotEqual(Operator.add(4, 5), 6)
+        self.assertEqual(Operator.mul(4, -5), -20)
+        self.assertNotEqual(Operator.mul(4, 5), 6)
 
-    # Defect Testing
+    # Defect Testing - floats
     def test_mul_floats(self):
         self.assertRaises(NotIntegerException, Operator.mul, 10.5, 2)
     
-    # Defect Testing
+    # Defect Testing -- strings
     def test_mul_strings(self):
         self.assertRaises(NotIntegerException, Operator.mul, 'abc', 'def')
-            
+
 if __name__ == '__main__':
     unittest.main()
